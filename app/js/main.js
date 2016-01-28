5edf78096a52bb2f23040956b6d83717b5ad3111
Vadim Derman <thenoodly@gmail.com>
2016-01-27 19:23:20 -0800

    without animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,50 @@
-(function(){
-	var
-		container 	= getElementByClass('accordeon__list'),
-		item		= container.child('accordeon__item');
+var accordeon = (function(){
+
+	function _setUpListeners() {
+		var
+			link = document.getElementsByClassName('accordeon__link'),
+			length = link.length,
+			i;
+		for( i=0; i < length; i++) {
+			_addListener(link[i], 'click', _openItem);
+		}
+	}
+
+	function _addListener(e, type, fn) {
+
+		if( typeof addEventListener !== 'undefined' ) {
+			e.addEventListener( type, fn, false);
+		} else if ( typeof attachEvent !== 'undefined' ) {
+			e.attachEvent( 'on' + type, fn );
+		} else {
+			e['on' + type] = fn;
+		}
+
+	}
+
+	function _openItem() {
+		var
+			$this 		= this,
+			container 	= document.getElementsByClassName('accordeon__list'),
+			items		= container[0].children,
+			item 		= $this.parentElement,
+			itemsLen 	= items.length,
+			i;
+
+		if ( !item.classList.contains('active') ) {
+
+			for( i=0; i<itemsLen; i++ ) {
+				items[i].classList.remove('active');
+			}
+			item.classList.add('active');
+		} else {
+			item.classList.remove('active');
+		}
+	}
+
+	return {
+		init: _setUpListeners
+	};
 }());
+
+accordeon.init();
